ca6db2583b205944a614b57693f11e8a808ec3af
yutom7 <yutom1411@gmail.com>
2026-02-08 20:24:28 +0900

    Remove tasks route and update AndroidManifest

--- app/src/main/java/com/yutom7/notionbucketlist/TodayTaskWidgetReceiver.kt
+++
@@ -1,8 +0,0 @@
-package com.yutom7.notionbucketlist
-
-import androidx.glance.appwidget.GlanceAppWidget
-import androidx.glance.appwidget.GlanceAppWidgetReceiver
-
-class TodayTaskWidgetReceiver : GlanceAppWidgetReceiver() {
-    override val glanceAppWidget: GlanceAppWidget = TodayTaskWidget()
-}
